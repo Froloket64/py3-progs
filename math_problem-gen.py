846bc00b8b5b71e4a5c3349cc08fa07e03128433
Froloket64 <59119389+Froloket64@users.noreply.github.com>
2021-04-23 16:17:19 +0000

    Some refactoring

--- math_problem-gen.py
+++ math_problem-gen.py
@@ -1,43 +1,40 @@
-# Условие :
-#
-# Нужно, чтобы:
-# 1. Генерировались числа от 1 до 10;       +
-# 2. Знак между ними ( +, -, *, / );        +
-# 3. Ответ не являлся отрицательным числом или дробью ( В любом проявлении ).                               + -
+###  Math problem Generator (for ~4-5th grades)  ###
 
-import random
+## Rules:
+## 1. Problems shouldn't have negative numbers neither floats
+## 2. Numbers should be <= 10
+## 3. Possible operations: +, -, *, /
+
+from random import randint, choice
 from operator import add, mul, truediv, sub
 from math import sqrt
 
-a = random.randint( 1, 11 )
-b = random.randint( 1, 11 )
+
+a = random.randint( 1, 10 )
+b = random.randint( 1, 10 )
 opers = [ add, sub, mul, truediv ]
 i = 0
 ii = 1000000
+
 
 for i in range( 0, ii ) :
     oper = random.choice( opers )
     ans = oper( a, b )
     oper1 = str( oper )
-    if ans == int( ans ) and ans == sqrt( ans**2 ) :
-        #print( 'Da' )
+    if ans == int( ans ) and ans == sqrt( ans**2 ):
         break
-    else :
-        #print( 'Net' )
+    else:
         continue
 
-if oper1 == '<built-in function truediv>' :
+
+if oper1 == '<built-in function truediv>':
     oper = '/'
-elif oper1 == '<built-in function add>' :
+elif oper1 == '<built-in function add>':
     oper = '+'
-elif oper1 == '<built-in function mul>' :
+elif oper1 == '<built-in function mul>':
     oper = '*'
-else :
+else:
     oper = '-'
 
-# Debug :
-# print( a, b )
-# print( ans )
 
-print ( a, oper, b, '=', ans )
-#print( 'x = ?' )
+print( a, oper, b, '=', ans )
