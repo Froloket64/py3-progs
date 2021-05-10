cff9044524d1d258e36d18c5c29aa47041d5fb71
Froloket64 <ket.frol2006@gmail.com>
2021-05-10 21:03:08 +0000

    wrapCheck: Rewrote, refactoring

--- wrapCheck.py
+++ wrapCheck.py
@@ -1,57 +1,46 @@
-###  A function that defines whether you can wrap a GIVEN word from a GIVEN point (index) or inside it.  ###
-
+###  A function that defines whether you can wrap a given word  ###
 
 ## Thoughts:
-# 1. If there are no vowels in the part of the word given, it can't be wrapped. [DONE]
-# 2. If there is only 1 vowel AND first character isn't a vowel AND the next one is, it'll not be wrapped.
-# 3. If the part has a vowel, after which we get a non-vowel AND a syllable of 3+ chars, it can be.
+# 1. If there is only 1- vowel --> False
 
 
-def wrapCheck(word, n):
+def wrapCheck(word):
     ## Words:
     # - kill
     # - do
     # - people
     # - sample
+    # - prerequisites
 
 
-    w1 = word[n:]
-    isHere = False
     vowels = 0
 
 
     if len(word) <= 5:  ## Pretty self-explanatory
         return False
-    if len(w1) <= 3:  ## Same here
+
+
+    for char in 'aeyou':  ## Check if there are any vowels in the word
+        vowels += word.count(char)  ## Count vowels there
+
+
+    if vowels <= 0:
         return False
 
-
-    for char in 'aeyou':  ## Check if there are any vowels after nth character in the word
-        if char in w1:
-            isHere = True
-            break
-
-        vowels += w1.count(char)  ## Count vowels in the part
-
-
-#     if 
-
-
-    if not isHere:  ## If there are no vowels (in the part), return False without further ado
-        return False
-#    if [word.count(char) for char in 'aeyou'] == 1:
 
     return True
 
 
+## ~Random Tests
 #for word in :
 #    wrapCheck()
 
-## ~Random Tests
-print( [wrapCheck('kill', 0),  ## GTG
-    wrapCheck('do', 0),  ## GTG
-    wrapCheck('people', 0),
-    wrapCheck('sample', 0)] )
-
 ## Fixed Tests
-print(wrapCheck('prepare', 3))
+print( [wrapCheck('kill'),  ## GTG
+    wrapCheck('do'),  ## GTG
+    wrapCheck('people'),
+    wrapCheck('sample'),
+    wrapCheck('prerequisites')] )
+
+## Add. Tests
+print(wrapCheck('prepare'))
